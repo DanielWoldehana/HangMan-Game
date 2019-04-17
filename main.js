cd142ac3a79a52e56952d8c8c1c861f77510127e
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-04-16 21:39:25 -0400

    canvas added as background

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-
-// let wordBank = require('random-words')
-
-// console.log(wordBank())
+let  wordBank = ['java', 'javascript', 'css']
 
 
+let categoryValue = document.querySelector("#category-select")
+categoryValue.addEventListener('click', function(evt){
+    if(categoryValue.value == "random"){
+      wordBank.push("Hello", "there")
+     }
+})
 
 
-let  wordBank = ['java is awesome', 'javascript', 'css']
+
+
+// let  wordBank = ['java', 'javascript', 'css']
 
 let random = Math.floor(Math.random() * wordBank.length)
 let myButton = document.querySelector(".myButton")
 
-myButton.addEventListener("click", function(){location.reload()})
+myButton.addEventListener("click", function(evt){
+    evt.preventDefault()
+    location.reload()
+})
 
 let randomWord = wordBank[random].toUpperCase()
 let myP = document.querySelector(".dashes")
@@ -78,3 +86,64 @@ let lifes = 6
 life.innerHTML = `❤️: 6`
 
 
+
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let c = canvas.getContext('2d');
+c.fillStyle = "blue";
+
+function Circle(x, y, dx, dy, radius, r, g, b){
+    this.x = x
+    this.y = y
+    this.dx = dx
+    this.dy = dy
+    this.radius = Math.floor(Math.random() * 30)
+   this.r = r
+   this.g = g
+   this.b = b
+    this.draw = function(){
+       c.beginPath();
+       c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+       c.strokeStyle = "white";
+       c.stroke();
+    }
+    this.update = function(){
+       if(this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+           this.dx = -this.dx
+       }
+   
+       if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
+           this.dy = -this.dy
+       }
+   
+       this.x += this.dx
+       this.y += this.dy
+       
+       this.draw()
+    }
+   }
+   
+   let circlesArray = [];
+   for(let i = 0; i < 100; i++){
+       let radius = 30
+       let x = Math.random() * (innerWidth - radius * 2) + radius
+       let y = Math.random() * (innerHeight - radius * 2) + radius
+       let dx = (Math.random() - 0.5) * 3
+       let dy = (Math.random() - 0.5) * 3
+       circlesArray.push(new Circle(x, y, dx, dy, radius))
+
+   }
+
+
+function animate() {
+   requestAnimationFrame(animate)
+   c.clearRect(0, 0, innerWidth, innerHeight)
+
+   for(let i = 0; i < circlesArray.length; i++){
+       circlesArray[i].update();
+   }
+}
+
+animate()
